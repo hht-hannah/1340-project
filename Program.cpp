fc8b223ee031ebfe59444e96fb3b25e6a89ce206
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-17 18:06:23 +0800

    Update Program.cpp

--- Program.cpp
+++ Program.cpp
@@ -3,12 +3,17 @@
 #include <string>
 using namespace std;
 
+const int GOOD_MAX=100;
 struct customer{
   double profit;
   int item_no;
 };
 void readlist(){
-  ifstream 
+  int good[GOOD_MAX];
+  ifstream fin("goods.txt");
+  isringstream word;
+  while(getline(fin,lin))
+  
 }
 
 void generate_random(){}
