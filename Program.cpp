e67dde1d848471f41cc186a7c4d712728f9e80d6
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-17 17:18:32 +0800

    Create Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void generate_random(){}
+
+void arrange_line(){}
+
+void print_line(){}
+
+int main()
+{
+  
+  ifstream
+}
